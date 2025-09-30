583fe91488d974a21077dbdaf4a30bcb520121af
Obiasogu Esther Chizaram <chizaramesther298@gmail.com>
2025-09-30 10:03:05 +0100

    updatd

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,         // allow network/remote hosts
-    port: 5173,         // ensure this matches your dev server port
-    strictPort: true,   // prevent Vite from picking another port
-    allowedHosts: 'all' // accept any host (ngrok included)
+    host: true,
+    port: 5173,
+    strictPort: true,
+    allowedHosts: [
+      'disintegrable-unrespited-asha.ngrok-free.dev'
+    ]
   }
 })
