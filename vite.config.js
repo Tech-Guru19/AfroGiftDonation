8c2d5005d53fea3885c2ed7a7e645f0ba5e65247
Obiasogu Esther Chizaram <chizaramesther298@gmail.com>
2025-09-30 10:28:04 +0100

    ahh

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: "0.0.0.0",
-    port: 5173
-  }
+    port: 5173,
+    allowedHosts: ["disintegrable-unrespited-asha.ngrok-free.dev"], // 👈 add your ngrok domain here
+  },
 });
