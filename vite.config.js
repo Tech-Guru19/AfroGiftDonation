23524ee1f72c09d04839d4fcdc2fcfe838efa800
Obiasogu Esther Chizaram <chizaramesther298@gmail.com>
2025-09-30 09:51:03 +0100

    corrected

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: 'all' // ✅ allow all hosts, including ngrok URLs
+    allowedHosts: 'all', // ✅ allow any host (ngrok works)
   }
 })
