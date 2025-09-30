8707e4e6fd64c3c9c5c7664e0725988aa55ecafe
Obiasogu Esther Chizaram <chizaramesther298@gmail.com>
2025-09-30 10:26:07 +0100

    ahhhhhhhhhhhh

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // ensures assets load correctly from any host
   server: {
-    host: true,
-    port: 5173,
-    strictPort: true,
-    allowedHosts: ['disintegrable-unrespited-asha.ngrok-free.dev'] // your ngrok URL
+    host: "0.0.0.0",
+    port: 5173
   }
 });
